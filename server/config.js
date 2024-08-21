bf157d349928a044b433e032a2c4ec77bb727669
Krishna Singha <krishnasingha.dev@gmail.com>
2024-08-21 22:44:50 +0530

    minor changes

--- server/config.js
+++ server/config.js
@@ -1,6 +1,8 @@
 // const FRONTEND_URL = "http://localhost:5173"
-const FRONTEND_URL = "https://www.ksingha.design"
-const BACKEND_URL = "http://localhost:8000"
+// const BACKEND_URL = "http://localhost:8000"
+
+const FRONTEND_URL = "https://www.krishnasingha.wiki"
+const BACKEND_URL = "https://krishna-portfolio-server.vercel.app"
 
 module.exports = {
     FRONTEND_URL,
