e6a1694c10f27907ccdf98d3ccb3393ccd2aa361
Krishna Singha <krishnasingha.dev@gmail.com>
2024-09-10 23:54:16 +0530

    updated model

--- server/index.js
+++ server/index.js
@@ -1,150 +1,143 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
 const port = process.env.PORT || 8000;
-const { FRONTEND_URL } = require('./config');
-const connectToMongoDB = require('./database/connection');
-const contactModel = require('./database/models/contact.model');
-const adminModel = require('./database/models/admin.model');
-const buildModel = require('./database/models/build.model');
+const { FRONTEND_URL } = require("./config");
+const connectToMongoDB = require("./database/connection");
+const contactModel = require("./database/models/contact.model");
+const adminModel = require("./database/models/admin.model");
+const buildModel = require("./database/models/build.model");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(
-    {
-        origin: FRONTEND_URL,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-    }
-));
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use("/", (req, res, next) => {
-    if (req.headers.origin === FRONTEND_URL) {
-        next();
-    } else {
-        res.status(403).json({
-            error: "Unauthorized",
-            message: `Sorry, you are not allowed to access this API.`,
-        });
-    }
+  if (req.headers.origin === FRONTEND_URL) {
+    next();
+  } else {
+    res.status(403).json({
+      error: "Unauthorized",
+      message: `Sorry, you are not allowed to access this API.`,
+    });
+  }
 });
 
 // Connect to MongoDB
 connectToMongoDB();
 
-app.get('/', (req, res) => {
-    res.send('Server is running!');
+app.get("/", (req, res) => {
+  res.send("Server is running!");
 });
 
-app.post('/admin', async (req, res) => {
-    try {
-        const { email, uid } = req.body;
-        console.log("Req", email, uid);
-        // const user = await adminModel.findOne({ email, uid });
-        // if (user) {
-        //     console.log("admin");
-        //     return res.status(200).send("admin");
-        // }
-        // console.log("not admin");
-        const getData = await adminModel.find({});
-        console.log("Res", getData);
-        return res.status(401).send("not an admin");
-    } catch (error) {
-        return res.status(500).send("Internal Server Error");
+app.post("/admin", async (req, res) => {
+  try {
+    const { email, uid } = req.body;
+    const user = await adminModel.findOne({ email, uid });
+    if (user) {
+      return res.status(200).send("admin");
     }
+    return res.status(401).send("not an admin");
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 // Route to add a new contact
-app.post('/contact', async (req, res) => {
-    try {
-        const { name, email, message } = req.body;
-        await contactModel.create({
-            name,
-            email,
-            message,
-        });
-        return res.sendStatus(201);
-    } catch (error) {
-        console.error("Error adding contact:", error);
-        return res.status(500).send("Internal Server Error");
-    }
+app.post("/contact", async (req, res) => {
+  try {
+    const { name, email, message } = req.body;
+    await contactModel.create({
+      name,
+      email,
+      message,
+    });
+    return res.sendStatus(201);
+  } catch (error) {
+    console.error("Error adding contact:", error);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 // Admin route to fetch contacts from backend
-app.post('/admin/contact', async (req, res) => {
-    try {
-        const { email, uid } = req.body;
-        const user = await adminModel.findOne({ email, uid });
-        if (!user) {
-            return res.status(401).send("Unauthorized");
-        }
-        const contacts = await contactModel.find({}).sort({ _id: -1 });
-        return res.status(200).send(contacts);
+app.post("/admin/contact", async (req, res) => {
+  try {
+    const { email, uid } = req.body;
+    const user = await adminModel.findOne({ email, uid });
+    if (!user) {
+      return res.status(401).send("Unauthorized");
     }
-    catch (error) {
-        return res.status(500).send("Internal Server Error");
-    }
+    const contacts = await contactModel.find({}).sort({ _id: -1 });
+    return res.status(200).send(contacts);
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
-
 // Route to add a new build
-app.post('/build', async (req, res) => {
-    try {
-        const { name, email, phone, projectDetails, budget } = req.body;
-        await buildModel.create({
-            name,
-            email,
-            phone,
-            projectDetails,
-            budget,
-        });
-        return res.sendStatus(201);
-    } catch (error) {
-        console.error("Error adding build:", error);
-        return res.status(500).send("Internal Server Error");
-    }
+app.post("/build", async (req, res) => {
+  try {
+    const { name, email, phone, projectDetails, budget } = req.body;
+    await buildModel.create({
+      name,
+      email,
+      phone,
+      projectDetails,
+      budget,
+    });
+    return res.sendStatus(201);
+  } catch (error) {
+    console.error("Error adding build:", error);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 // Route to get all build messages
-app.post('/admin/build', async (req, res) => {
-    try {
-        const { email, uid } = req.body;
-        const user = await adminModel.findOne({ email, uid });
-        if (!user) {
-            return res.status(401).json("Unauthorized");
-        }
-        const build = await buildModel.find({}).sort({ _id: -1 });
-        return res.status(200).json(build);
-    } catch (error) {
-        return res.status(500).json("Internal Server Error");
+app.post("/admin/build", async (req, res) => {
+  try {
+    const { email, uid } = req.body;
+    const user = await adminModel.findOne({ email, uid });
+    if (!user) {
+      return res.status(401).json("Unauthorized");
     }
+    const build = await buildModel.find({}).sort({ _id: -1 });
+    return res.status(200).json(build);
+  } catch (error) {
+    return res.status(500).json("Internal Server Error");
+  }
 });
 
 // Admin route to delete a contact
-app.delete('/admin/contact/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        await contactModel.findOneAndDelete({ _id: id });
-        return res.status(200).send("Deleted");
-    } catch (error) {
-        return res.status(500).send("Internal Server Error");
-    }
+app.delete("/admin/contact/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await contactModel.findOneAndDelete({ _id: id });
+    return res.status(200).send("Deleted");
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
 });
 // Admin route to delete a build
-app.delete('/admin/build/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        await buildModel.findOneAndDelete({ _id: id });
-        return res.status(200).send("Deleted");
-    } catch (error) {
-        return res.status(500).send("Internal Server Error");
-    }
+app.delete("/admin/build/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await buildModel.findOneAndDelete({ _id: id });
+    return res.status(200).send("Deleted");
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
