7f84a891d75d5b0e61d2d09322a2742140b3b034
Krishna Singha <krishnasingha.dev@gmail.com>
2024-09-10 23:56:27 +0530

    minor change

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:8000"
+// export const BACKEND_URL = "http://localhost:8000"
 
-// export const BACKEND_URL = "https://krishna-portfolio-server.vercel.app"
+export const BACKEND_URL = "https://krishna-portfolio-server.vercel.app"
     
